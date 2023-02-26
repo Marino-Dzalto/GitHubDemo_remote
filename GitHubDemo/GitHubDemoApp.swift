c5cc45d5d7e37d876dc1b0a5947f210e75e67711
DZALTO <marino.dzalto@icloud.com>
2023-02-26 16:02:50 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Marino Dzalto on 26.02.2023..
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
