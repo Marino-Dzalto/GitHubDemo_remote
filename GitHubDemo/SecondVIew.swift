eea73cdabec97a04151c01a63aafb54878acded4
DZALTO <marino.dzalto@icloud.com>
2023-02-26 16:08:42 +0100

    Create SecondView

---
+++ GitHubDemo/SecondVIew.swift
@@ -0,0 +1,20 @@
+//
+//  SecondVIew.swift
+//  GitHubDemo
+//
+//  Created by Marino Dzalto on 26.02.2023..
+//
+
+import SwiftUI
+
+struct SecondVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondVIew()
+    }
+}
